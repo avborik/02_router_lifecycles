f1cd21e343a2cf712643157aa1d5ff0a4288d686
AvBorikDeveloper <avboris28@gmail.com>
2019-04-22 12:33:44 +1200

    Creating routers

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+
+// components
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profile';
+
+
+const App= () => {
+    return (
+       <BrowserRouter>
+            <div>
+                <header>
+                    Header
+                </header>
+                <Route path="/" exact component={Home}/>
+                <Route path="/posts" component={Posts}/>
+                <Route path="/profile" component={Profile}/>
+            </div>
+       </BrowserRouter>
+    )
+} 
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
