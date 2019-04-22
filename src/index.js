bea58b4c22e1db9392146c54ad5e404c79860b6b
AvBorikDeveloper <avboris28@gmail.com>
2019-04-22 22:45:53 +1200

    params and urls

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link, NavLink} from 'react-router-dom';
 
 
 // components
 import Home from './components/home';
 import Posts from './components/posts';
 import Profile from './components/profile';
+import PostItem from './components/post_item';
 
 
 const App= () => {
@@ -14,17 +15,22 @@ const App= () => {
        <BrowserRouter>
             <div>
                 <header>
-                    <Link to="/">Home</Link><br/>
-                    <Link to="/posts">Posts</Link><br/>
-                    <Link to={{
+                    <NavLink to="/">Home</NavLink><br/>
+                    <NavLink 
+                            to="/posts"
+                            activeStyle={{color:'red'}}
+                            activeClassName='selected'
+                            >Posts</NavLink><br/>
+                    <NavLink to={{
                         pathname:'/profile',
                         // hash:'#Boris',
                         // search: '?profile=true'
-                    }}>Profile</Link><br/>
+                    }}>Profile</NavLink><br/>
                     <hr/>
                 </header>
                     <Route path="/" exact component={Home}/>
-                    <Route path="/posts" component={Posts}/>
+                    <Route path="/posts" exact component={Posts}/>
+                    <Route path="/posts/:id/:username" component={PostItem}/>
                     <Route path="/profile" component={Profile}/>
             </div>
        </BrowserRouter>
