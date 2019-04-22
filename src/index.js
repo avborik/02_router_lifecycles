0013b590b2ee6978246d46d08d8528a3c8d04a4e
AvBorikDeveloper <avboris28@gmail.com>
2019-04-22 23:07:45 +1200

    Routers switch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Link, NavLink} from 'react-router-dom';
+import {BrowserRouter, Route, Link, NavLink, Switch} from 'react-router-dom';
 
 
 // components
@@ -28,10 +28,13 @@ const App= () => {
                     }}>Profile</NavLink><br/>
                     <hr/>
                 </header>
-                    <Route path="/" exact component={Home}/>
-                    <Route path="/posts" exact component={Posts}/>
-                    <Route path="/posts/:id/:username" component={PostItem}/>
-                    <Route path="/profile" component={Profile}/>
+                    <Switch>
+                        <Route path="/posts/:id/:username" component={PostItem}/>
+                        <Route path="/profile" component={Profile}/>
+                        <Route path="/posts" component={Posts}/>
+                        <Route path="/" component={Home}/>
+                    </Switch>
+                   
             </div>
        </BrowserRouter>
     )
